f8dd951e5042bb491d068c8b113beb2645a817e0
Colin de Groot <colindegroot2004@gmail.com>
2024-09-02 12:34:35 +0200

    Eigen aanpasingen verwerkt in het kaartje
    kleine aanpassingen in de css, html en comments in de js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') //selecteer de button in het html bestand
+expandButton.addEventListener('click', expand) // voeg de event click toe aan de expand functie
 
-function expand () {
-  document.body.classList.toggle('expand')
+function expand () { // aanmaken van de functie expand 
+  document.body.classList.toggle('expand') // een toggle toevoegen aan de expand zodat je in en uit kan zoomen
 }
