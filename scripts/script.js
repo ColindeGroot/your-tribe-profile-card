56126120b4382f70caae0464fea90c7dba06636b
Colin de Groot <colindegroot2004@gmail.com>
2024-09-03 10:50:36 +0200

    Nieuwe kaart gemaakt
    Een eigen visitekaart gemaakt met html,css en js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-const expandButton = document.querySelector('header button') //selecteer de button in het html bestand
-expandButton.addEventListener('click', expand) // voeg de event click toe aan de expand functie
-
-function expand () { // aanmaken van de functie expand 
-  document.body.classList.toggle('expand') // een toggle toevoegen aan de expand zodat je in en uit kan zoomen
-}
+document.querySelector('.card-container').addEventListener('click', function () {
+  const card = document.querySelector('.card');
+  if (card.style.transform === 'rotateX(180deg)') {
+      card.style.transform = 'rotateX(0deg)';
+  } else {
+      card.style.transform = 'rotateX(180deg)';
+  }
+  console.log(card)
+});
