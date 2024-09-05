144bed0fbfcda8fa5caba379667fee75e863ef30
Colin de Groot <colindegroot2004@gmail.com>
2024-09-05 22:41:36 +0200

    Finished product
    Product is af maar kan nog altijd worden uitgebreid. Met deze versie kan het kaartje worden opgeleverd/beoordeeld.

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,42 @@ document.querySelector('.card-container').addEventListener('click', function () 
   } else {
       card.style.transform = 'rotateX(180deg)';
   }
-  console.log(card)
+  
+  document.querySelectorAll('.theme-select').forEach(button => { 
+    button.addEventListener('click', (e) => {
+      e.stopPropagation();
+      const section = button.getAttribute('data-section');
+      showContent(section);
+    });
+  });
 });
+
+function showContent(section) {
+  document.querySelector('.back h2').style.display = 'none';
+  document.querySelector('.button-list').style.display = 'none';
+
+  document.querySelectorAll('.section-content').forEach(content => {
+    content.style.display = 'none'; 
+  });
+  
+  document.getElementById(section).style.display = 'block'; 
+  document.querySelector('.content').classList.add('visible'); 
+  
+
+  document.querySelectorAll('.back-button').forEach(button => {
+    button.addEventListener('click', (e) => {
+      e.stopPropagation();
+      hideContent();
+    });
+  });
+}
+
+function hideContent() {
+  document.querySelector('.content').classList.remove('visible'); 
+  document.querySelectorAll('.section-content').forEach(content => {
+    content.style.display = 'none'; 
+  });
+  
+  document.querySelector('.back h2').style.display = 'block';
+  document.querySelector('.button-list').style.display = 'block';
+}
