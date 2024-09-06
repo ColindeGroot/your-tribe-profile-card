ae4d748552d2413798ea4384dbb0aaaf3a158434
Colin de Groot <colindegroot2004@gmail.com>
2024-09-06 13:02:32 +0200

    kleine p erbij

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 document.querySelector('.card-container').addEventListener('click', function () {
   const card = document.querySelector('.card');
   if (card.style.transform === 'rotateX(180deg)') {
-      card.style.transform = 'rotateX(0deg)';
+    card.style.transform = 'rotateX(0deg)';
   } else {
-      card.style.transform = 'rotateX(180deg)';
+    card.style.transform = 'rotateX(180deg)';
   }
-  
-  document.querySelectorAll('.theme-select').forEach(button => { 
+
+  document.querySelectorAll('.theme-select').forEach(button => {
     button.addEventListener('click', (e) => {
       e.stopPropagation();
       const section = button.getAttribute('data-section');
@@ -20,12 +20,12 @@ function showContent(section) {
   document.querySelector('.button-list').style.display = 'none';
 
   document.querySelectorAll('.section-content').forEach(content => {
-    content.style.display = 'none'; 
+    content.style.display = 'none';
   });
-  
-  document.getElementById(section).style.display = 'block'; 
-  document.querySelector('.content').classList.add('visible'); 
-  
+
+  document.getElementById(section).style.display = 'block';
+  document.querySelector('.content').classList.add('visible');
+
 
   document.querySelectorAll('.back-button').forEach(button => {
     button.addEventListener('click', (e) => {
@@ -36,11 +36,11 @@ function showContent(section) {
 }
 
 function hideContent() {
-  document.querySelector('.content').classList.remove('visible'); 
+  document.querySelector('.content').classList.remove('visible');
   document.querySelectorAll('.section-content').forEach(content => {
-    content.style.display = 'none'; 
+    content.style.display = 'none';
   });
-  
+
   document.querySelector('.back h2').style.display = 'block';
   document.querySelector('.button-list').style.display = 'block';
 }
